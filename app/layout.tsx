ca56baf35d1acbdcc68e5a7952f50f8aded960f9
jgansl <jessegansler@gmail.com>
2026-01-18 19:14:56 -0800

    gsap + shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import "styles/tailwind.css"
 
 import SmoothScroll from "components/SmoothScroll"
+import Footer from "@/components/Footer"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        <SmoothScroll>{children}</SmoothScroll>
+      <body className="dark">
+        <SmoothScroll>
+          <main>{children}</main>
+          <Footer />
+        </SmoothScroll>
       </body>
     </html>
   )
