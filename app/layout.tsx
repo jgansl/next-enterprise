1536ac3bdb4e8e888176fd7b325caf1405a8c931
jgansl <jessegansler@gmail.com>
2026-01-18 19:22:18 -0800

    setup/AppNavigationMenu

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,14 @@ import "styles/tailwind.css"
 
 import SmoothScroll from "components/SmoothScroll"
 import Footer from "@/components/Footer"
+import { AppNavigationMenu } from "@/components/AppNavigationMenu"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="dark">
         <SmoothScroll>
+          <AppNavigationMenu />
           <main>{children}</main>
           <Footer />
         </SmoothScroll>
